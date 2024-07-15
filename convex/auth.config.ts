1e93eaaaa8bb03eedd490947f6e88bd6a7f2cccb
Diasnk <diasnugumanov258@gmail.com>
2024-07-15 10:17:24 +0500

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+        domain: "https://fitting-lab-50.clerk.accounts.dev",
+        applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig
